a35aaaf9eb069375abed378963b430aaaef0fbba
Emily Curtin <emily.curtin@weather.com>
2016-06-28 14:38:53 -0400

    added worksheets to the gitignore, deleted a bunch of dead comments and code

--- src/main/scala/com/weather/streamsx/cassandra/TupleToStatement.scala
+++ src/main/scala/com/weather/streamsx/cassandra/TupleToStatement.scala
@@ -116,7 +116,7 @@ object TupleToStatement {
     case "ustring" => tuple.getString(attr.index)
     case "blob" => tuple.getBlob(attr.index)
     case "xml" => tuple.getXML(attr.index).toString //Cassandra doesn't have XML as data type, thank goodness
-    case "list" => getListWithProperType(tuple, attr)
+    case s if s.startsWith("list") => getListWithProperType(tuple, attr)
     //I wonder if there will need to be more specific qualifications with list<boolean>, list<int>, etc
     case "map" => tuple.getMap(attr.index) //same dubiosity for maps as for lists
 //    case "tuple" => tuple.getTuple(attr.index)
@@ -132,30 +132,4 @@ object TupleToStatement {
       case "Int" => rawList.asInstanceOf[java.util.List[Int]].toList
     }
   }
-
-//  def getBoundStatement(list: List[Attr], ps: PreparedStatement): BoundStatement = {
-//    values =
-//
-//    return null
-//  }
-
-/*
-
-tuples come in from streams along with some indication of whether each value is null
-
-  I can massage this format into a list of case class Attr()s
-
-  From these Attr()s I need to create a prepared statement using only the fields that are not null
-    There's going to be a lot of repitition in these prepared statements, there should be some caching
-    The cache can be a LRU style where the presence of nulls in the statement maps to a bit string, and the bit string is what's compared.
-    I'm distracted thinking about some debugging here for when fields change and/or the number of fields changes
-      The first time a tuple comes in, hash the field names into some UUID.
-      Check each tuple's field names against this hash.
-      If the two hashes are ever not equal, replace the hash and invalidate the bitmask cache because there's new/different fields.
-  When I have the prepared statement, I can turn that into a bound statement by binding it to the values that are not null from the tuple
-  I can have a session that asynchronously inserts the bound statement into Cassandra
-
-*/
-
-
 }
