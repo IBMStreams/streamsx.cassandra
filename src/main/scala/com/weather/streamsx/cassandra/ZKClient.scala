afe0fbab8fb5409c68e602e21932f2c78f9ede83
Emily Curtin <emily.curtin@weather.com>
2016-07-15 10:24:56 -0400

    hate to do this, but changing the default port on zkclient to what it actually is on the VM. feels like cheating

--- src/main/scala/com/weather/streamsx/cassandra/ZKClient.scala
+++ src/main/scala/com/weather/streamsx/cassandra/ZKClient.scala
@@ -3,7 +3,7 @@ package com.weather.streamsx.cassandra
 import com.weather.streamsx.util.zookeeper.{ZkCfg, ZkCli}
 
 object ZkClient {
-  private val cfg = ZkCfg("streamsx.cassandra", sys.env.getOrElse("STREAMS_ZKCONNECT", "localhost:2181"))
+  private val cfg = ZkCfg("streamsx.cassandra", sys.env.getOrElse("STREAMS_ZKCONNECT", "localhost:21810"))
   val zkCli = ZkCli(cfg)
   val zk = zkCli.client
 }
