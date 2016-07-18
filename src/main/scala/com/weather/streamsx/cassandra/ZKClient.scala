e3d2bebbf3ce7f07a667befcdc7fec2cb7b29cb8
Emily Curtin <emily.curtin@weather.com>
2016-07-18 12:25:58 -0400

    testing

--- src/main/scala/com/weather/streamsx/cassandra/ZKClient.scala
+++ src/main/scala/com/weather/streamsx/cassandra/ZKClient.scala
@@ -3,7 +3,7 @@ package com.weather.streamsx.cassandra
 import com.weather.analytics.zooklient.{ZooKlient, ZooKlientCfg}
 
 object ZKClient {
-  private val cfg = ZooKlientCfg("streamsx.cassandra", sys.env.getOrElse("STREAMS_ZKCONNECT", "localhost:21810"))
+  private val cfg = ZooKlientCfg("streamsx.cassandra", sys.env.getOrElse("STREAMS_ZKCONNECT", "localhost:2181"))
   val zkCli = ZooKlient(cfg)
   val zk = zkCli.client
 }
