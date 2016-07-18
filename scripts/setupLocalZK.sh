abb53b322e5394a8d88a572b467360b79cda4bbc
Emily Curtin <emily.curtin@weather.com>
2016-07-18 15:39:39 -0400

    delete znode if already there

--- scripts/setupLocalZK.sh
+++ scripts/setupLocalZK.sh
@@ -2,4 +2,6 @@
 
 cfg=$(<cfg.json)
 
+zookeepercli --servers localhost:21810 -c delete /streamsx.cassandra/hello_world_info
+
 zookeepercli --servers localhost:21810 -c creater /streamsx.cassandra/hello_world_info "$cfg"
