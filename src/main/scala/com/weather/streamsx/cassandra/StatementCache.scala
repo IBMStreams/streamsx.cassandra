edaf5a9496526fd6ad3b96539bbdd6bc7e83fe96
Emily Curtin <emily.curtin@weather.com>
2016-07-28 10:46:05 -0400

    debugging

--- src/main/scala/com/weather/streamsx/cassandra/StatementCache.scala
+++ src/main/scala/com/weather/streamsx/cassandra/StatementCache.scala
@@ -10,13 +10,15 @@ class StatementCache(args: CassSinkClientConfig, session: Session, masterList: D
     override def load(key: BitSet): PreparedStatement = loaderFn(key)
   }
 
-  private val loaderFn = (b: BitSet) => {
+  private[cassandra] val loaderFn = (b: BitSet) => {
     val keys = b.map(masterList(_)).toList
     val fieldStr = keys.mkString(",")
+    println(s"fieldStr $fieldStr")
     val q = ("?" * keys.length).mkString(",")
+    println(s"q: $q")
     val tableSpec = if (args.keyspace.isEmpty) args.table else s"${args.keyspace}.${args.table}"
     val insertStr = s"""INSERT INTO $tableSpec ($fieldStr) VALUES ($q) USING TTL ${args.ttl}"""
-    //    println(insertStr)
+    println(s"insertStr: $insertStr")
     session.prepare(insertStr)
   }
 
---
+++ src/test/scala/com/weather/streamsx/cassandra/StatementCacheTest/scala.scala
@@ -0,0 +1,10 @@
+package com.weather.streamsx.cassandra
+
+
+//ToDO disable these freaking created by auto generatred things
+/**
+  * Created by ecurtin on 7/28/16.
+  */
+class StatementCacheTest {
+
+}
