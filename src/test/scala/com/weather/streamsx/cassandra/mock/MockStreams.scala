37203df65fbefde08d72b49f3d63bf3b7fbc784b
Emily Curtin <emily.curtin@weather.com>
2016-08-17 17:43:19 -0400

    debugging, think I'm close, just need to get the driver reconnection messages to quit

--- src/test/scala/com/weather/streamsx/cassandra/mock/MockStreams.scala
+++ src/test/scala/com/weather/streamsx/cassandra/mock/MockStreams.scala
@@ -55,4 +55,6 @@ class MockStreams(splStyleTupleStructureDeclaration: String) {
 
   // omg can I actually get a tuple out of this???
   def newEmptyTuple(): OutputTuple = injector.newTuple()
+
+  testableGraph.shutdown().get().shutdown().get()
 }
