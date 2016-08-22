3ba1ed8956287c6e1c901a22f89c053c9bfcb683
Emily Curtin <emily.curtin@weather.com>
2016-08-22 17:31:39 -0400

    if you're happy and you know it, syntax error

--- src/test/scala/com/weather/streamsx/cassandra/GreetingTest.scala
+++ src/test/scala/com/weather/streamsx/cassandra/GreetingTest.scala
@@ -5,7 +5,7 @@ class GreetingTest extends PipelineTest(
   "testk",
   "testt",
   """
-     |create table testk.testt IF NOT EXISTS testkeyspace.testtable (
+     |create table IF NOT EXISTS testk.testt (
      |  greeting varchar,
      |  count bigint,
      |  testList list<int>,
@@ -31,9 +31,14 @@ class GreetingTest extends PipelineTest(
                           )
 
     val tuple, valuesMap = genAndSubmitTuple(structureMap)
-    
+
     val rows = session.execute(s"select * from $keyspace.$table").all.asScala.toSeq
     rows should have size 1
   }
 
 }
+
+/*
+I like Ryan a lot and today is his birthday
+Hello world I donh'rt reall ty likr this keybooard because the keys are not in the riught place and this for me is vberyu frustrating and also my hands are not centered very well so it's even more frustrating and I have no idea how I would feelk enteriung numbers siuce I usualluy use only one hand for that.nl
+ */
