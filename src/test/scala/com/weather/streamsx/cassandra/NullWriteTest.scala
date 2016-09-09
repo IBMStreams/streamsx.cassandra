17c44088394604d57563c1822debd1ce7f65ce00
Emily Curtin <emily.curtin@weather.com>
2016-09-09 17:04:51 -0400

    added null test

---
+++ src/test/scala/com/weather/streamsx/cassandra/NullWriteTest.scala
@@ -0,0 +1,97 @@
+package com.weather.streamsx.cassandra
+
+import com.datastax.driver.core.Row
+import com.ibm.streams.operator.{OutputTuple, Tuple}
+import com.weather.streamsx.cassandra.mock.MockStreams
+
+import scala.collection.JavaConverters._
+
+  object NullWriteTest {
+    val table = "testkEEEE"
+    val keyspace = "testtAAAA"
+    val tableStr =   """
+                       |create table IF NOT EXISTS testk.testt (
+                       |  count bigint,
+                       |  notnull int,
+                       |  nullz int,
+                       |  PRIMARY KEY (count)
+                       |) with caching = 'none';
+                     """.stripMargin
+
+    val nullVals =
+      s"""
+         |{
+         |  "nullz" : ${Int.MaxValue}
+         |}
+       """.stripMargin
+  }
+
+
+class NullWriteTest extends PipelineTest(
+  NullWriteTest.table,
+  NullWriteTest.keyspace,
+  NullWriteTest.tableStr,
+  NullWriteTest.nullVals
+){
+
+  override def beforeAll(): Unit = {
+    println("I'M CALLING BEFOREALL FOR GREETING TEST!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    super.beforeAll()
+  }
+
+  override def afterAll(): Unit = {
+    println("I'M CALLING AFTERALL FOR GREETING TEST!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+    super.afterAll()
+  }
+
+    val structureMap = Map( "count" -> "uint64",
+                            "notnull" -> "int32",
+                            "nullz" -> "int32"
+                          )
+    def row2greeting(r: Row): Map[String, Any] = {
+      Map(
+        "count" -> r.getLong("count"),
+        "notnull" -> r.getInt("notnull")
+
+      )
+    }
+
+  "The operator" should "write only one tuple to C*" in {
+    val (tuple, valuesMap) = genTuple(structureMap)
+
+
+    def genTuple(m: Map[String, String]): (Tuple, Map[String, Any]) = {
+      val tupleStructure = {
+        val tupleOpen = "tuple<"
+        val meat = m.map(kv => s"${kv._2} ${kv._1}").mkString(", ")
+        val tupleClose = ">"
+        s"$tupleOpen$meat$tupleClose"
+      }
+      println(s"THIS IS MY TUPLE STRUCTURE: $tupleStructure")
+      val generator = new MockStreams(tupleStructure, mockZK.connectString)
+      val t = generator.newEmptyTuple()
+
+      t.setLong("count", 12345678L)
+      t.setInt("notnull", Int.MaxValue)
+      t.setInt("nullz", Int.MaxValue)
+
+      generator.submit(t)
+
+      (t, Map(
+        "count" -> 12345678L,
+        "notnull" -> Int.MaxValue,
+        "nullz" -> Int.MaxValue
+      ))
+
+    }
+
+    val rows: Seq[Row] = session.execute(s"select * from $keyspace.$table").all.asScala.toSeq
+    val received = row2greeting(rows.head)
+
+    rows should have size 1
+    received("count") shouldBe valuesMap("count")
+    received("notnull") shouldBe valuesMap("notnull")
+    received("nullz") shouldBe null
+  }
+}
