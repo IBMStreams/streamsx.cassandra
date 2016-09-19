40314a15fabc2cd8d90832b86a67980ceda23c5e
Emily Curtin <emily.curtin@weather.com>
2016-09-19 18:26:15 -0400

    removed warning on DualHash

--- src/main/scala/com/weather/streamsx/cassandra/DualHash.scala
+++ src/main/scala/com/weather/streamsx/cassandra/DualHash.scala
@@ -6,13 +6,23 @@ class DualHash(list: List[Attribute]) extends DualHashy{
   val nameToInt: Map[String, Int] = list.map(a => a.getName -> a.getIndex).toMap
   val intToName: Map[Int, String] = list.map(a => a.getIndex -> a.getName).toMap
 
-  def apply(s: String): Option[Int] = nameToInt(s) match {
-    case i: Int => Some(i)
-    case _ => None
+  def apply(s: String): Option[Int] = {
+    try Some(nameToInt(s))
+    catch {
+      case e: Exception => None
+    }
   }
-  def apply(i: Int): Option[String] = intToName(i) match {
-    case s: String if s.nonEmpty => Some(s)
-    case _ => None
+
+  def apply(i: Int): Option[String] = {
+    try {
+      intToName(i) match {
+        case s: String if s.nonEmpty => Some(s)
+        case _ => None
+      }
+    }
+    catch {
+      case e: Exception => None
+    }
   }
 }
 
