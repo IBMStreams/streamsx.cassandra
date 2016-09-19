5e8718323116a0842e1b830136217f1eb382d7fc
Emily Curtin <emily.curtin@weather.com>
2016-09-19 10:08:42 -0400

    removing cassandra unit

--- src/test/scala/com/weather/streamsx/cassandra/mock/MockCassandra.scala
+++ src/test/scala/com/weather/streamsx/cassandra/mock/MockCassandra.scala
@@ -1,46 +1,9 @@
-package com.weather.streamsx.cassandra
-
-import org.cassandraunit.utils.{EmbeddedCassandraServerHelper => helper}
-import org.junit.runner.RunWith
-import org.scalatest.{BeforeAndAfterAll, Matchers, FlatSpec}
-import org.scalatest.junit.JUnitRunner
+package com.weather.streamsx.cassandra.mock
 
 // I have yet to find a library for embedded Cassandra for unit testing that doesn't SUCK.
 // I'm running cassandra locally while I run tests, which is not ideal, but it's better than these buggy Cassandra libraries.
 
 object MockCassandra {
-
-////  private val helper = EmbeddedCassandraServerHelper
-//
-//  def start(): Unit = helper.startEmbeddedCassandra("test-cassandra.yml")
-//
-//  //  def stop(): Unit = helper.stopEmbeddedCassandra() // The stop method was deprecated in cassandra-unit with no replacement
-//  def clean(): Unit = helper.cleanEmbeddedCassandra()
-//
-//  start()
-//  val ip: String = helper.getHost
-//  val port: Int = helper.getNativeTransportPort // This is the CQL port
-
   val ip = "127.0.0.1,10.0.2.2"
   val port = 9042
 }
-
-//
-//@RunWith(classOf[JUnitRunner])
-//class MockCassandraTest extends FlatSpec with Matchers with BeforeAndAfterAll {
-//  override def beforeAll() {
-//    MockCassandra.start()
-//  }
-//
-//  override def afterAll() {
-//    MockCassandra.clean()
-//  }
-//
-//  "MockCassandra" should "be running" in {
-//    MockCassandra.ip should be
-//    MockCassandra.port should be
-//  }
-//
-//
-//
-//}
